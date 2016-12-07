92fe29db94ab3ae538187d004e93bc298760b553
Dustin Rowland <lee.rowland@gmail.com>
2016-12-07 08:40:21 -0600

    Update backupDroplet.sh

--- backupDroplet.sh
+++ backupDroplet.sh
@@ -3,12 +3,12 @@
 rm /home/drowland/backup.tar.gz
 
 # Backup MySQL database phpipam
-#mysqldump -u root -p7f4878BAb/*- syspass > /home/drowland/syspass.sql
-mysqldump -u root -p7f4878BAb/*- recipebook > /home/drowland/recipebook.sql
-#mysqldump -u root -p7f4878BAb teampass > /root/teampass.sql
+#mysqldump -u root -pROOTPWD syspass > /home/drowland/syspass.sql
+mysqldump -u root -pROOTPWD recipebook > /home/drowland/recipebook.sql
+#mysqldump -u root -pROOTPWD teampass > /root/teampass.sql
 
 # Restore MySQL database phpipam
-#mysql -u root -p7f4878BAb phpipam < phpipam.sql
+#mysql -u root -pROOTPWD phpipam < phpipam.sql
 
 #Archive backup script, database export, and wwwdir
 tar -zcf backup.tar.gz --exclude=/var/www/html/piwik backupDroplet.sh README resume.json recipebook.sql /var/www/html /etc/apache2/sites-available /etc/apache2/.htpasswd
